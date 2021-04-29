b79fa029cbf9ecf4984a3d9369362d4372d80725
Victor Marra <zaffalonvictor@gmail.com>
2021-04-28 22:18:14 -0300

    Remove build folder

--- build/src/store/subcategories.store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const seed_1 = require("./seed");
-/**
- * In-Memory Store
- */
-let sub_categories = seed_1.subcategories;
-exports.default = sub_categories;
