c9ce11614e8e05f985401011ed4d10b2fe640a95
Victor Marra <zaffalonvictor@gmail.com>
2021-04-30 01:39:05 -0300

    Fix dockerfile and update gitignore to allow env file

--- build/src/store/categories.store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const seed_1 = require("./seed");
-/**
- * In-Memory Store
- */
-let categories = seed_1.categories;
-exports.default = categories;
